34ab95318a96a4e8ac440e91dc0afda37c34c5a5
Kirill Prakhov <kirya_prahov@mail.ru>
2024-01-23 15:53:06 +0400

    fix: change movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -16,7 +16,32 @@ public class Player : MonoBehaviour
     void Update()
     {
         Vector3 moveDir = gameInput.GetMovementDirectonVectorNormalized();
-        transform.position += moveDir * speed * Time.deltaTime;
+
+        float playerSize = .1f;
+        float playerRadius = .7f;
+        float playerHeight = 2f;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, playerSize);
+        if(!canMove) {
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0);
+            moveDirX.Normalize();
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, playerSize);
+
+            if(canMove) {
+                moveDir = moveDirX;
+            } else {
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z);
+                moveDirZ.Normalize();
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, playerSize);
+
+                if(canMove) {
+                    moveDir = moveDirZ;
+                }                
+            }
+        }
+        if(canMove) {
+            transform.position += moveDir * speed * Time.deltaTime;
+        }
+        
 
         isWalking = moveDir != Vector3.zero;
 
